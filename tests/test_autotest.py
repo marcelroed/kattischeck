657d46a79cee91403dc802b900ca7a371e4c2b60
marcelroed <marcelroed@gmail.com>
2019-01-14 13:52:21 +0100

    Tests for fetching and running scripts

--- tests/test_autotest.py
+++ tests/test_autotest.py
@@ -1,2 +1,22 @@
-class TestRun:
-    pass
+from kattis_autotest.fetch_data import get_zip_file, unzip
+from kattis_autotest.run_script import run_script, script_paths
+import os
+
+
+class TestFetchData:
+    def test_fetch_zip(self):
+        data_url = 'https://open.kattis.com/problems/doorman/file/statement/samples.zip'
+        data = get_zip_file(data_url)
+        files = unzip(data)
+
+
+class TestRunScript():
+    def test_run_local(self):
+        script_path = os.path.curdir + '/aaah.py'
+        result = run_script(script_path, 'aaah\naah')
+        print(result)
+        assert result == 'go\n'
+
+    def test_find_paths(self):
+        paths = script_paths('aaah')
+        print(paths)
