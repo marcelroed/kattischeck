5ead46cf53fc59e9f2f83b3ec8a3f3587484d2ea
marcelroed <marcelroed@gmail.com>
2019-01-15 19:51:10 +0100

    Flag for clearing screen

--- kattischeck/__main__.py
+++ kattischeck/__main__.py
@@ -1,5 +1,8 @@
 import argparse
 import sys
+from kattischeck.kattis_check import kattis_check
+from os import system
+import platform
 
 vprint = None
 
@@ -7,9 +10,15 @@ vprint = None
 def main(args=sys.argv[1:]):
     # Parse args
     parsed_args = parse_args(args)
-    print(parsed_args)
     # Setup verbose printing, can call vprint()
     init_vprint(parsed_args.verbose)
+    if parsed_args.clear:
+        os = platform.system()
+        if os == 'Windows':
+            system('cls')
+        else:
+            system('clear')
+    kattis_check(parsed_args.problem_names)
 
 
 def init_vprint(verbose):
@@ -21,6 +30,7 @@ def parse_args(args):
     parser = argparse.ArgumentParser(description="Check Kattis solution")
     parser.add_argument('problem_names', metavar='problem-name', help='the name of the problem(s)', type=str, nargs='+')
     parser.add_argument('--verbose', help='increase output verbosity', action='store_true')
+    parser.add_argument('--clear', help='clear console window before running', action='store_true')
     return parser.parse_args(args)
 
 
