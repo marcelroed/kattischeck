73457aae4b77ddd34f6f7b969c1aa8e20150fa02
marcelroed <marcelroed@gmail.com>
2019-01-15 19:49:53 +0100

    Allow multiple implementations (Python and C++) by using a buffer instead of a generator for reading.

--- kattischeck/fetch_data.py
+++ kattischeck/fetch_data.py
@@ -19,9 +19,13 @@ def unzip(zip_file):
     :return: List of [name, file_stream]
     """
     name_list = zip_file.namelist()
-    print(name_list)
-    return list(zip(name_list, [decode(zip_file.open(file_name).readlines()) for file_name in zip_file.namelist()]))
+    return list(zip(name_list, [[line for line in decode(zip_file.open(file_name).readlines())] for file_name in
+                                zip_file.namelist()]))
 
 
 def decode(byte_str_generator):
     return (line.decode('UTF-8') for line in byte_str_generator)
+
+
+def get_unzipped_file(url):
+    return unzip(get_zip_file(url))
